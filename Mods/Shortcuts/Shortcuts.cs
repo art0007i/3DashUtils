4098aaa7c9d3b6b7cafe581b9ac31b2cfc0ed5ed
Andrew <alexfeed1990real@gmail.com>
2023-06-17 19:00:17 +0300

    shortcuts menu offline levels needs fixing

---
+++ Mods/Shortcuts/Shortcuts.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using _3DashUtils.ModuleSystem;
+using UnityEngine.UI;
+
+namespace _3DashUtils.Mods.Shortcuts;
+
+internal class Shortcuts : ModuleBase
+{
+    public override string CategoryName => "Shortcuts";
+
+    private Dictionary<string, string> shortcuts = new() {
+        {
+            "Main Menu", "Menu"
+        },
+        {
+            "Online Levels", "Online Levels Hub"
+        },
+        {
+            "Level Editor", "Save Select"
+        }
+    };
+
+    public override void OnGUI()
+    {
+        foreach(var shortcut in shortcuts)
+        {
+            if(GUILayout.Button(shortcut.Key))
+            {
+                SceneManager.LoadScene(shortcut.Value);
+            }
+        }
+
+        if(GUILayout.Button("Offline Levels"))
+        {
+            SceneManager.LoadScene("Menu");
+            var menuManager = Object.FindObjectOfType<MenuButtonScript>();
+            menuManager.panner.anchoredPosition = Utils.ChangeX(menuManager.pannerTransformGoal, 0f - menuManager.movementDistance);
+            menuManager.pannerTransformGoal = Utils.ChangeX(menuManager.pannerTransformGoal, 0f - menuManager.movementDistance);
+        }
+    }
+}
