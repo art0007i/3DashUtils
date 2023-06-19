b22dc1099546ef4f2f22053a7dc5b0c6698635db
Andrew <alexfeed1990real@gmail.com>
2023-06-19 09:57:05 +0300

    mroe defauly shit

---
+++ Mods/Player/PracticeCheckpointFix.cs
@@ -0,0 +1,17 @@
+﻿using _3DashUtils.ModuleSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DashUtils.Mods.Player;
+
+public class PracticeCheckpointFix : ToggleModule
+{
+    public override string CategoryName => "Player";
+
+    protected override bool Default => true;
+
+    public override string Description => "Fixes practice checkpoint input buffering and fixes placing checkpoints while dead.";
+}
