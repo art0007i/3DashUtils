835a7bfb9af39cb6d4fcab30450ed78d55b02dfb
art0007i <art0007i@gmail.com>
2023-06-20 11:53:06 +0200

    fix nullref exception move config ui generation to a different function move ui style to a different class

--- ModuleSystem/Config/IConfigurableModule.cs
+++ ModuleSystem/Config/IConfigurableModule.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace _3DashUtils.ModuleSystem.Config;
 
 public interface IConfigurableModule
 {
-    List<IConfigOption> ConfigOptions { get; }
+    public List<IConfigOption> ConfigOptions { get; }
+}
+
+public static class ConfigurableModuleExtensions
+{
+    public static void BuildConfigGUI(this IConfigurableModule module)
+    {
+        
+            foreach (var configOp in module.ConfigOptions)
+            {
+
+                GUILayout.BeginHorizontal(new GUIContent("", Extensions.GenerateTooltip(configOp)), GUIStyle.none, GUILayout.ExpandHeight(false));
+                GUILayout.Label(configOp.Name, GUILayout.ExpandWidth(true));
+                //GUILayout.BeginHorizontal(GUILayout.Width(50f));
+                configOp.OnGUI();
+                //GUILayout.EndHorizontal();
+                GUILayout.EndHorizontal();
+            }
+        
+    }
 }
