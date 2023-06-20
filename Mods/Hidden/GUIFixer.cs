adae43dcd39824fb80e24cd512971e693382e657
art0007i <art0007i@gmail.com>
2023-06-20 23:23:36 +0200

    fixed placing blocks on ui

---
+++ Mods/Hidden/GUIFixer.cs
@@ -0,0 +1,24 @@
+﻿using _3DashUtils.ModuleSystem;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace _3DashUtils.Mods.Hidden;
+
+public class GUIFixer : ModuleBase
+{
+    public override string CategoryName => "Hidden";
+}
+
+[HarmonyPatch(typeof(FlatEditor), "NullifyIfOverUI")]
+class FlatEditorPatch
+{
+    public static void Postfix(ref bool ___overUI)
+    {
+        ___overUI |= GUIUtility.hotControl != 0;
+    }
+}
