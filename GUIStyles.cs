976e8dbcb0a50fddac0c7d2c9c927b5a0f5cd5de
art0007i <art0007i@gmail.com>
2023-06-20 11:53:06 +0200

    fix nullref exception move config ui generation to a different function move ui style to a different class

---
+++ GUIStyles.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace _3DashUtils;
+
+public class GUIStyles
+{
+    private static GUIStyle tooltip;
+    public static GUIStyle Tooltip
+    {
+        get
+        {
+            if (tooltip == null)
+            {
+                tooltip = new GUIStyle(GUI.skin.button);
+                tooltip.fontSize = 16;
+            }
+            return tooltip;
+        }
+    }
+    private static GUIStyle configButton;
+    public static GUIStyle ConfigButton
+    {
+        get
+        {
+            if (configButton == null)
+            {
+                configButton = new GUIStyle(GUI.skin.button);
+                configButton.alignment = TextAnchor.MiddleCenter;
+            }
+            return configButton;
+        }
+    }
+    private static GUIStyle configLayout;
+    public static GUIStyle ConfigLayout
+    {
+        get
+        {
+            if (configLayout == null)
+            {
+                // keeping this for when I want to re-style the ui maybe?
+                configLayout = new GUIStyle(GUI.skin.box);
+            }
+            return configLayout;
+        }
+    }
+}
