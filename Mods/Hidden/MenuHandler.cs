add6c627d4d479e4488f8c4d29c9726cfd8e4924
art0007i <art0007i@gmail.com>
2023-06-23 11:14:08 +0200

    add keybind conflicts add menu open fallback (i hate that feature)

--- Mods/Hidden/MenuHandler.cs
+++ Mods/Hidden/MenuHandler.cs
@@ -11,28 +11,40 @@ namespace _3DashUtils.Mods.Hidden;
 public class MenuHandler : ModuleBase, IKeybindModule
 {
     public static ConfigEntry<bool> menuOpen = _3DashUtils.ConfigFile.Bind("Main", "MenuOpen", true);
+    public static KeyBindInfo menuOpenKeyBind;
     public override string CategoryName => "Hidden";
 
     public List<KeyBindInfo> KeyBinds { get; private set; } = new();
 
     public MenuHandler()
     {
-        KeyBinds.Add(new("MenuOpenKey", () => menuOpen.Value = !menuOpen.Value, "The key that opens and closes the utility menu.", KeyCode.Tab));
+        menuOpenKeyBind = new("MenuOpenKey", () => menuOpen.Value = !menuOpen.Value, "The key that opens and closes the utility menu.", KeyCode.Tab);
+        KeyBinds.Add(menuOpenKeyBind);
     }
 
     public override void Update()
     {
-        if(_3DashUtils.currentKeybindEditing is KeyBindEditInfo i && i.editingFinished)
+        if (_3DashUtils.currentKeybindEditing == null)
+        {
+            if (MenuHandler.menuOpenKeyBind.KeyBind == KeyCode.None)
+            {
+                _3DashUtils.menuOpenFallback = true;
+                _3DashUtils.EditKey(new(KeyCode.None, (key) => MenuHandler.menuOpenKeyBind.KeyBind = key, "Open Menu"));
+                return;
+            }
+            Extensions.CollectKeyBindInfos().Do((bind) =>
+            {
+                if (Input.GetKeyDown(bind.KeyBind))
+                {
+                    bind.KeyCallback();
+                }
+            });
+        }
+        if (_3DashUtils.currentKeybindEditing.editingFinished)
         {
             _3DashUtils.currentKeybindEditing = null;
-            return;
+            _3DashUtils.conflicts = null;
+            _3DashUtils.menuOpenFallback = false;
         }
-        _3DashUtils.moduleList.OfType<IKeybindModule>().SelectMany((k) => k.KeyBinds).Do((bind) =>
-        {
-            if (Input.GetKeyDown(bind.KeyBind))
-            {
-                bind.KeyCallback();
-            }
-        });
     }
 }
